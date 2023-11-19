2e74cbe5166f07621901f3d2e0c8e917ca195b5a
skeletontonguedworld <129410383+skeletontonguedworld@users.noreply.github.com>
2023-11-19 14:09:28 +0100

    Sea Of Emptiness
    (dodana pętelka i pytanko, czy w ogóle kcesz, bo wiadomo, zgoda najważniejsza)

--- letthingsdrift.nim
+++ letthingsdrift.nim
@@ -1,26 +1,36 @@
 import std/random
 randomize()
 import os
+import std/strutils
 
 var wielkiearkana = ["0 Głupiec", "I Mag", "II Kapłanka", "III Cesarzowa", "IV Cesarz", "V Arcykapłan", "VI Kochankowie", "VII Rydwan", "VIII Moc", "IX Eremita", "X Koło Fortuny", "XI Sprawiedliwość", "XII Wisielec", "XIII Śmierć", "XIV Umiarkowanie", "XIX Słońce", "XV Diabeł", "XVI Wieża", "XVII Gwiazda", "XVIII Księżyc", "XX Sąd ostateczny", "XXI Świat"]
-var małearkana = ["As", "Król", "Królowa", "Rycerz", "Paź", "Dziesiątka", "Dziewiątka", "Ósemka", "Siódemka", "Szóstka", "Piątka,", "Czwórka", "Trójka", "Dwójka"]
+var małearkana = ["As", "Król", "Królowa", "Rycerz", "Paź", "Dziesiątka", "Dziewiątka", "Ósemka", "Siódemka", "Szóstka", "Piątka", "Czwórka", "Trójka", "Dwójka"]
 var kolory = ["Buław", "Denarów", "Mieczy", "Pucharów"]
 
-echo "Witam, Twoja karta na dziś to:"
+while true:
 
-shuffle(wielkiearkana)
-shuffle(małearkana)
-var karta = sample(wielkiearkana)
-var mkarta = sample(małearkana)
+  echo "Witam, czy chcesz wylosować kartę tarota?"
+  let me_die = readLine(stdin)
 
-var wybór = [karta, mkarta]
-var final = sample(wybór)
+  if me_die.contains("ak"):
+    echo "Twoja karta na dziś to:"
 
-if final == mkarta:
-  shuffle(kolory)
-  let kolor = sample(kolory)
-  echo final, " ", kolor
-else:
-  echo final
+    shuffle(wielkiearkana)
+    shuffle(małearkana)
+    var karta = sample(wielkiearkana)
+    var mkarta = sample(małearkana)
 
-sleep(5000)
+    var wybór = [karta, mkarta]
+    var final = sample(wybór)
+
+    if final == mkarta:
+      shuffle(kolory)
+      let kolor = sample(kolory)
+      echo final, " ", kolor
+    else:
+      echo final
+
+    sleep(5000)
+  else: 
+    echo "Okok, szegnam"
+    break
